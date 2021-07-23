970bf49127737c0d1a6b80585e7f3f21856fb465
Arav Ashish <87805464+Arav2010@users.noreply.github.com>
2021-07-23 19:13:11 +0530

    Add files via upload

---
+++ Days of Week.py
@@ -0,0 +1,17 @@
+day=int(input("Enter the Day Number : "))
+if day==1:
+  print("MONDAY")
+elif day==2:
+  print("TUESDAY")
+elif day==3:
+  print("WEDNESDAY")
+elif day==4:
+  print("THURSDAY")
+elif day==5:
+  print("FRIDAY")
+elif day==6:
+  print("SATURDAY")
+elif day==7:
+  print("SUNDAY")
+else:
+  print("This is an invalid number. Please write between 1 to 7")
